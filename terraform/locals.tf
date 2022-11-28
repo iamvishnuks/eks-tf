9f3b00e2e5c620f92eb67d7e9fe4430f9c89b52f
vishnu.kuttantharappelsoman <vishnu.kuttantharappelsoman@sportion.net>
2022-11-28 12:54:55 +0530

    corrected cluster name

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  cluster_name = "toptal-test"
+  cluster_name = "test"
   subnet_ids = ["${aws_subnet.prod-subnet-private-app-1a.id}", "${aws_subnet.prod-subnet-private-db-1a.id}", "${aws_subnet.prod-subnet-private-mon-1a.id}", "${aws_subnet.prod-subnet-private-app-1b.id}", "${aws_subnet.prod-subnet-private-db-1b.id}", "${aws_subnet.prod-subnet-private-mon-1b.id}"]
 }
